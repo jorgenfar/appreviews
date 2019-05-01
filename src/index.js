edcd1226b29dab6850d7cf504378c5c6b2448153
Jørgen Faret <jorgen.faret@netlight.com>
2019-05-01 21:43:22 +0200

    Add nicer formatting to review printing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { pollAppStore } = require('./app-store-service');
+const stars = (rating) => '⭐️'.repeat(rating);
 
 pollAppStore()
-    .subscribe(review => console.log(review.body + '\n\n\n'));
+    .subscribe(review => console.log(stars(review.rating) + '\n' + review.title + '\n' + review.body + '\n\n'));
