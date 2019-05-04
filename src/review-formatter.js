6c9fd4632c5ac4c811839a2c3084eee1d6728b96
Jørgen Faret <jorgen.faret@netlight.com>
2019-05-04 03:32:41 +0200

    Update formatting

--- src/review-formatter.js
+++ src/review-formatter.js
@@ -1,15 +1,15 @@
 const stars = rating => '⭐️'.repeat(rating);
 
 const formatReview = review => ({
-    channel: 'general',
     attachments: [
         {
             mrkdwn_in: ['text', 'pretext', 'title'],
             color: review.rating > 3 ? 'good' : (review.rating >= 2 ? 'warning' : 'danger'),
             author_name: review.userName,
-            title: stars(review.rating),
+            title: `${stars(review.rating)} - ${review.title} `,
             text: review.body,
-            footer: 'footer'
+            footer: `Versjon ${review.appVersion} - <${review.link}|App Store>`,
+            footer_icon: 'https://emojis.slackmojis.com/emojis/images/1450319442/24/appleinc.png?1450319442',
         }
     ]
 });
