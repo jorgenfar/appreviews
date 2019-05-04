54c297e69c53b813c02dd80b8e7b42fd5c40b497
Jørgen Faret <jorgen.faret@netlight.com>
2019-05-04 13:44:02 +0200

    Add pm2 and pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+// Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+
+module.exports = {
+  apps : [{
+    name: 'appreviews',
+    script: 'src/index.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'app',
+      host : '68.183.223.4',
+      ref  : 'origin/master',
+      repo : 'git@github.com:jorgenfar/appreviews.git',
+      path : '/home/app/appreviews',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
