dd4fd38e6df05036dfbf7dbe0a0fdc2d302051c5
Jørgen Faret <jorgen.faret@netlight.com>
2019-05-09 14:18:34 +0200

    Attempt to fix stars not showing correctly on mobile

--- src/review-formatter.js
+++ src/review-formatter.js
@@ -1,19 +1,22 @@
 const { appleIcon, androidIcon } = require('../config');
 const { platFormDependentString } = require('./utils/platform-utils');
 
-const stars = rating => '⭐️'.repeat(rating);
+const stars = rating => `${':star:️'.repeat(rating)}`;
 
-const formatTitle = review =>
+const title = review =>
     `${stars(review.rating)}${review.title ? ` - ${review.title}` : ''}`;
+
+const footer = review =>
+    `Versjon ${review.appVersion} - <${review.link}|${platFormDependentString(review.platform, 'Play', 'App')} Store>`;
 
 const formatReview = review => ({
     attachments: [
         {
             color: review.rating > 3 ? 'good' : (review.rating >= 2 ? 'warning' : 'danger'),
             author_name: review.userName,
-            title: formatTitle(review),
+            title: title(review),
             text: review.body,
-            footer: `Versjon ${review.appVersion} - <${review.link}|${platFormDependentString(review.platform, 'Play', 'App')} Store>`,
+            footer: footer(review),
             footer_icon: platFormDependentString(review.platform, androidIcon, appleIcon),
         }
     ]
